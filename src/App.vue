904870ab61339d9c728baedb572ebf3c5bf04e3f
hnin-ei-phyu <nanghnineiphyu2001@gmail.com>
2023-12-10 22:29:43 +0630

    first

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+  import {ref} from "vue";
+
+  const count = ref(0);
+</script>
+<template>
+  <main>
+    <div>
+      <h2>The Current Count is...</h2>
+      <h1>{{ count }}</h1>
+      <button @click="count--">-</button>&nbsp;&nbsp;
+      <button @click="count++">+</button>
+    </div>
+  </main>
+
+</template>
+
+<style scoped>
+  main {
+    width: 100vw;
+    height: 100vh;
+    background-color: skyblue;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  div{
+    text-align: center;
+  }
+</style>
